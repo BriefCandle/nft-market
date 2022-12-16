3f4f469f42fc39e98d98c059c1ad3248c8f7f298
yufan ling <yufanling@yufans-MacBook-Pro.local>
2022-12-15 22:08:50 -0800

    Creator fee test added

--- test/MarketCreator.t.sol
+++ test/MarketCreator.t.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
 import {console} from "forge-std/console.sol";
+import "src/example/ERC20Token.sol";
 import 'src/example/NFT2.sol';
 import 'src/example/NFTOwnable.sol';
 import 'src/Market.sol';
@@ -13,6 +14,7 @@ contract MarketCreatorTest is Test {
     Market market_nft;
     Market market_nft_ownable;
     MarketFactory factory;
+    ERC20Token erc20;
 
     address internal alice;
     address internal bob;
@@ -32,23 +34,24 @@ contract MarketCreatorTest is Test {
 
         market_nft = Market(factory.createMarket(address(nft)));
         market_nft_ownable = Market(factory.createMarket(address(nft_ownable)));
+        erc20 = new ERC20Token("a", "b");
     }
 
     function testNOTOwnableCannotSetCreator() public {
         vm.prank(alice);
         vm.expectRevert(bytes("Market: no creator setup"));
-        market_nft.setCreatorFee(charlie, 2);
+        market_nft.setCreatorFee(charlie, 200);
     }
 
     function testOwnableCanSetCreatorOnlyByCreator() public {
         // creator is successfully set
         vm.prank(bob);
-        market_nft_ownable.setCreatorFee(charlie, 2);
+        market_nft_ownable.setCreatorFee(charlie, 200);
         assertEq(market_nft_ownable.fee_recipient(), charlie);
         // not creator cannot set creator
         vm.prank(alice);
         vm.expectRevert(bytes("Market: not creator"));
-        market_nft_ownable.setCreatorFee(charlie, 2);
+        market_nft_ownable.setCreatorFee(charlie, 200);
     }
 
     function testOwnerTransferReset() public {
@@ -58,8 +61,164 @@ contract MarketCreatorTest is Test {
         market_nft_ownable.setCreatorFee(charlie, 2);
         vm.stopPrank();
         vm.prank(charlie);
-        market_nft_ownable.setCreatorFee(bob, 2);
+        market_nft_ownable.setCreatorFee(bob, 200);
         assertEq(market_nft_ownable.fee_recipient(), bob);
-
     }
+
+    /** ------ FEE CALCULATION ----- */
+    // bob is the creator; alice is the owner; charlie bids for alice's nft
+    function testBidAcceptCreatorReceiveERC20() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // charlie bids for the nft
+        uint256 bidPrice = 1000;
+        erc20.mint(charlie, uint256(1000));
+        vm.prank(charlie);
+        require(erc20.approve(address(market_nft_ownable), bidPrice));
+        vm.prank(charlie);
+        market_nft_ownable.buyerBid(1, bidPrice, address(erc20), uint32(1000));
+        // alice accpets charlie's bid
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAcceptBid(1, charlie);
+        vm.stopPrank();
+        // test
+        assertEq(erc20.balanceOf(alice), uint256(980)); // seller 
+        assertEq(erc20.balanceOf(bob), uint256(20)); // fee recipient
+        assertEq(erc20.balanceOf(charlie), uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+    // same as above except no creator setup 
+    function testNoCreatorBidERC20() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        // market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // charlie bids for the nft
+        uint256 bidPrice = 1000;
+        erc20.mint(charlie, uint256(1000));
+        vm.prank(charlie);
+        require(erc20.approve(address(market_nft_ownable), bidPrice));
+        vm.prank(charlie);
+        market_nft_ownable.buyerBid(1, bidPrice, address(erc20), uint32(1000));
+        // alice accpets charlie's bid
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAcceptBid(1, charlie);
+        vm.stopPrank();
+        // test
+        assertEq(erc20.balanceOf(alice), uint256(1000)); // seller 
+        assertEq(erc20.balanceOf(bob), uint256(0)); // fee recipient
+        assertEq(erc20.balanceOf(charlie), uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+    // bob is the creator; alice is the owner who asks for a price; charlie is the acceptor and buyer
+    function testAskAcceptCreatorReceiveETH() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // alice asks for a price
+        uint256 bidPrice = 1000; // in eth
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAsk(1, bidPrice, address(0), 1000);
+        vm.stopPrank();
+        // charlie accepts alice's ask
+        vm.deal(charlie, bidPrice);
+        vm.prank(charlie);
+        market_nft_ownable.buyerAcceptAsk{value: bidPrice}(1, charlie);
+        // test
+        assertEq(alice.balance, uint256(980)); // seller
+        assertEq(bob.balance, uint256(20)); // fee recipient
+        assertEq(charlie.balance, uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+    // bob is the creator; alice is the owner who asks for a price; charlie is the acceptor and buyer
+    function testNoCreatorAskETH() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        // market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // alice asks for a price
+        uint256 bidPrice = 1000; // in eth
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAsk(1, bidPrice, address(0), 1000);
+        vm.stopPrank();
+        // charlie accepts alice's ask
+        vm.deal(charlie, bidPrice);
+        vm.prank(charlie);
+        market_nft_ownable.buyerAcceptAsk{value: bidPrice}(1, charlie);
+        // test
+        assertEq(alice.balance, uint256(1000)); // seller
+        assertEq(bob.balance, uint256(0)); // fee recipient
+        assertEq(charlie.balance, uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+    //  same as above but with erc20 instead
+    function testAskAcceptCreatorReceiveERC20() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // alice asks for a price
+        uint256 bidPrice = 1000; // in eth
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAsk(1, bidPrice, address(erc20), 1000);
+        vm.stopPrank();
+        // charlie accepts alice's ask
+        erc20.mint(charlie, bidPrice);
+        vm.prank(charlie);
+        require(erc20.approve(address(market_nft_ownable), bidPrice));
+        vm.prank(charlie);
+        market_nft_ownable.buyerAcceptAsk(1, charlie);
+        // test
+        assertEq(erc20.balanceOf(alice), uint256(980)); // seller 
+        assertEq(erc20.balanceOf(bob), uint256(20)); // fee recipient
+        assertEq(erc20.balanceOf(charlie), uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+     //  same as above but with no creator setup
+    function testNoCreatorERC20() public {
+        // set bob to be the creator fee recipient
+        vm.prank(bob);
+        // market_nft_ownable.setCreatorFee(bob, 200); // 2% creator fee
+        // mint an nft to alice
+        nft_ownable.mintTo(alice);
+        // alice asks for a price
+        uint256 bidPrice = 1000; // in eth
+        vm.startPrank(alice);
+        nft_ownable.setApprovalForAll(address(market_nft_ownable), true);
+        market_nft_ownable.sellerAsk(1, bidPrice, address(erc20), 1000);
+        vm.stopPrank();
+        // charlie accepts alice's ask
+        erc20.mint(charlie, bidPrice);
+        vm.prank(charlie);
+        require(erc20.approve(address(market_nft_ownable), bidPrice));
+        vm.prank(charlie);
+        market_nft_ownable.buyerAcceptAsk(1, charlie);
+        // test
+        assertEq(erc20.balanceOf(alice), uint256(1000)); // seller 
+        assertEq(erc20.balanceOf(bob), uint256(0)); // fee recipient
+        assertEq(erc20.balanceOf(charlie), uint256(0)); // buyer
+        assertEq(nft_ownable.ownerOf(1), charlie); // new owner
+    }
+
+
+
+
 }
